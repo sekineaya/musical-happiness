2701c5bf57c81d81a75b6b55d063b3f16f5c4158
sekineaya <t236065@st.yamagata-u.ac.jp>
2024-10-11 19:11:15 +0900

    Add files via upload

---
+++ kanateya.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("kanateya LOVE");
+     
+     int kanateya;
+     kanateya = 315;
+     
+     printf("kanateya : %d",kanateya + kanateya);
+     return 0;
+}
