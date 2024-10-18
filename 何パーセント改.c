228ba3cc484bded0cfc188bc7e494a12b0fcd3ea
sekineaya <t236065@st.yamagata-u.ac.jp>
2024-10-18 19:27:50 +0900

    Add files via upload

---
+++ 何パーセント改.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int k;
+    printf("勇気何パーセント？");
+    scanf("%d", &k);
+    
+    int s;
+    printf("僕の勇気も足してね！");
+    scanf("%d", &s);
+    
+    printf("それはもちろん:%dパーセント!!", k+s);
+
+    return 0;
+}
