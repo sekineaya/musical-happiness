663b314513e90c3797f80003a3a025ee31f2ad99
sekineaya <t236065@st.yamagata-u.ac.jp>
2025-01-24 19:04:41 +0900

    Add files via upload

---
+++ rpg.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(0));
+    
+    int intelligence = (rand()%20) + 1;
+    int stamina      = (rand()%20-intelligence) + 1;
+    int charisma     = (rand()%20-intelligence- charisma ) + 1;
+    
+    printf("%d",intelligence);
+    printf("%d",stamina);
+    printf("%d",charisma); 
+    
+    if(intelligence>stamina&intelligence>charisma)
+    {
+        printf("mage");
+    }
+     
+    if(stamina>intelligence&stamina>charisma)
+    {
+        printf("knight");
+    }
+    
+    if(charisma>stamina&charisma>intelligence)
+    {
+        printf("thief");
+    }
+    
+    return 0;
+}
